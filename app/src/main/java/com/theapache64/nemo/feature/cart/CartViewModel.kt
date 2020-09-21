9138ddea91adced07c4d4bf2c6aa29c64f1bfe8b
theapache64 <theapache64@gmail.com>
2020-09-21 22:39:30 +0530

    🌄 init amountPayable logic

--- app/src/main/java/com/theapache64/nemo/feature/cart/CartViewModel.kt
+++ app/src/main/java/com/theapache64/nemo/feature/cart/CartViewModel.kt
@@ -6,6 +6,7 @@ import com.theapache64.nemo.data.repository.CartRepo
 import com.theapache64.nemo.data.repository.ConfigRepo
 import com.theapache64.nemo.feature.base.BaseViewModel
 import com.theapache64.nemo.utils.calladapter.flow.Resource
+import kotlinx.coroutines.flow.onEach
 
 /**
  * Created by theapache64 : Sep 19 Sat,2020 @ 14:52
@@ -18,13 +19,23 @@ class CartViewModel @ViewModelInject constructor(
     val config = configRepo.getLocalConfig()!!
     private val _shouldLoadCart = MutableLiveData<Boolean>()
 
+    private val _amountPayable = MutableLiveData<Int>()
+    val amountPayable: LiveData<Int> = _amountPayable
+
     val cartItems: LiveData<Resource<List<CartItem>>> = _shouldLoadCart.switchMap {
-        cartRepo.getCartItems().asLiveData(viewModelScope.coroutineContext)
+        cartRepo.getCartItems()
+            .onEach {
+                if (it is Resource.Success) {
+                    
+                }
+            }
+            .asLiveData(viewModelScope.coroutineContext)
     }
 
     init {
         loadCart()
     }
+
 
     fun loadCart() {
         _shouldLoadCart.value = true
