3d88a1d69349f6cbe87f4463ed07a28b5d872b6f
theapache64 <theapache64@gmail.com>
2020-11-20 23:36:36 +0530

    🌄 init productactivitytest

---
+++ app/src/androidTest/java/com/theapache64/nemo/feature/products/ProductsActivityTest.kt
@@ -0,0 +1,14 @@
+package com.theapache64.nemo.feature.products
+
+import junit.framework.TestCase
+import org.junit.Test
+
+/**
+ * Created by theapache64 : Nov 19 Thu,2020 @ 23:59
+ */
+class ProductsActivityTest {
+    @Test
+    fun ifBannerLoads(){
+
+    }
+}
