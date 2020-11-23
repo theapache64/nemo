cc89cf6c02b95300982fee913d7fc76eb32e6380
theapache64 <theapache64@gmail.com>
2020-11-23 23:36:47 +0530

    ◀️ backup

--- app/src/test/java/com/theapache64/nemo/feature/home/HomeViewModelTest.kt
+++ app/src/test/java/com/theapache64/nemo/feature/home/HomeViewModelTest.kt
@@ -2,6 +2,7 @@ package com.theapache64.nemo.feature.home
 
 import junit.framework.TestCase
 import org.junit.Test
+import org.mockito.Mockito.mock
 
 /**
  * Created by theapache64 : Nov 23 Mon,2020 @ 23:19
@@ -9,6 +10,9 @@ import org.junit.Test
 class HomeViewModelTest {
     @Test
     fun `Click on banner launch product`(){
-
+        val homeViewModel = HomeViewModel(
+            mock(),
+            mock()
+        )
     }
 }
